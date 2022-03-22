32341293ef898a4f39cee110d05a21761be15278
ross-weir <29697678+ross-weir@users.noreply.github.com>
2022-03-22 21:53:54 +1100

    init proj/scripts

---
+++ codegen.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+while getopts ":l:" opt; do
+  case "$opt" in
+    l)
+      lang="${OPTARG}"
+    ;;
+    :)
+      echo "option is missing required argument: '-${OPTARG}' (language)"
+      exit 1
+    ;;
+    ?)
+      echo "unrecognised option: '-${OPTARG}'"
+    ;;
+  esac
+done
+
+# Run the provided sh script with args if it exists
+run_if_exists() {
+  if [ -f "$1" ]; then
+    "$1" ${@:2}
+  fi
+}
+
+out_dir="clients/${lang}"
+cfg_dir="configs/${lang}"
+scripts_dir="configs/${lang}/scripts"
+
+git_user_id=wallet-ex
+git_repo_id=ergo-api-clients
+
+cfg_file="${cfg_dir}/config.yaml"
+cfg_flag="" && [[ -f $cfg_file ]] && cfg_flag="-c ${cfg_file}"
+templates_dir="${cfg_dir}/templates"
+templates_flag="" && [[ -d $templates_dir ]] && templates_flag="-t $templates_dir"
+
+# before generate script hook
+spec_sha="006676fde3ac8c544a0b526448eb6c54c4f567c8"
+spec="https://raw.githubusercontent.com/coinbase/rosetta-specifications/${spec_sha}/api.json"
+spec_file="${cfg_dir}/api.json"
+
+echo "downloading spec ${spec} to ${spec_file}"
+curl "${spec}" --output "${spec_file}"
+
+run_if_exists "${scripts_dir}/process_spec.sh" "${PWD}/${spec_file}"
+
+generator_version=$(cat "${cfg_dir}/gen_version")
+
+docker run --user "$(id -u):$(id -g)" --rm -v $(pwd):/local --workdir /local \
+openapitools/openapi-generator-cli:${generator_version} generate \
+-v \
+-i $spec \
+-g $lang \
+-o $out_dir \
+--git-user-id ${git_user_id} \
+--git-repo-id ${git_repo_id} \
+$cfg_flag \
+$templates_flag;
+
+# after generate script hook
+
+# publish script hook
---
+++ configs/typescript/scripts/process_spec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+spec_path=$1
+echo "${spec_path}"
